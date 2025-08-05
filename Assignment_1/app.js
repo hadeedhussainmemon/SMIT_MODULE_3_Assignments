6bc19bc6a299e078a139f6a4effc61be893fdd2c
geek-hadeed <programmerhadeed@gmail.com>
2025-08-05 10:31:26 +0100

    folder as module

---
+++ Assignment_1/app.js
@@ -0,0 +1,6 @@
+const {sum, sub, div, multi} = require('./calculate');
+
+console.log(sum(2,3))
+console.log(sub(98,3))
+console.log(div(32,9))
+console.log(multi(3,87))
--- Assignment_1/calculate/index.js
+++ Assignment_1/calculate/index.js
@@ -3,10 +3,4 @@ const {sub} = require('./sub.js');
 const {div} = require('./div.js');
 const {multi} = require('./multi.js');
 
-let a = prompt('Enter first number: ')
-let b = prompt('Enter second number: ')
-
-sum(a,b)
-sub(a,b)
-div(a,b)
-multi(a,b)
+module.exports = {sum, sub, div, multi};
